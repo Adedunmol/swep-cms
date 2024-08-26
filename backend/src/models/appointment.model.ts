2da1f996782c5907c7b71f1eb114568dd40adef4
Adedunmola Oyewale <oyewaleadedunmola@gmail.com>
2024-08-26 19:44:03 +0100

    add files for appointment

---
+++ backend/src/models/appointment.model.ts
@@ -0,0 +1,28 @@
+import db from "../database/database";
+
+interface CreateAppointment {
+    doctorId: string
+    email: string
+    phoneNumber: string
+    availableDay: string
+    availableMonth: string
+    reason: string
+}
+
+class Appointment {
+    async createAppointment(data: CreateAppointment) {
+        const [id] = await db('appointment').insert({
+            ...data
+        }).returning('id')
+
+        return { appointmentId: id }
+    }
+
+    async findAppointment(id: string) {
+        const appointment = await db('appointment').where({ id })
+
+        return appointment[0]
+    }
+}
+
+export default new Appointment()
