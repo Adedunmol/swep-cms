379c93fbe738d9409b0b103a973c9a19ae69e9b9
Adedunmola Oyewale <oyewaleadedunmola@gmail.com>
2024-08-31 15:00:30 +0100

    fix

--- backend/src/database/database.ts
+++
@@ -1,8 +0,0 @@
-import knex from 'knex'
-import config from './knexfile'
-
-const configEnv = process.env.ENVIRONMENT === 'development' ? config.development : config.production
-
-const db = knex(configEnv)
-
-export default db
