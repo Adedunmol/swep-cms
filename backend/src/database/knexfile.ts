f566aed54f78d7b01222ce373205922c02ac5a59
Adedunmola Oyewale <oyewaleadedunmola@gmail.com>
2024-09-04 19:57:41 +0100

    add prod to knexfile

--- backend/src/database/knexfile.ts
+++ backend/src/database/knexfile.ts
@@ -14,8 +14,25 @@ const config: { [key: string]: Knex.Config } = {
       user: process.env.DATABASE_USER,
       database: process.env.DATABASE_NAME,
       password: process.env.DATABASE_PASSWORD,
+    },
   },
-}
+  production: {
+    client: 'pg',
+    connection: {
+      host: process.env.DATABASE_HOST,
+      port: +process.env.DATABASE_PORT!!,
+      user: process.env.DATABASE_USER,
+      database: process.env.DATABASE_NAME,
+      password: process.env.DATABASE_PASSWORD,
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
 };
 
 export default config;
