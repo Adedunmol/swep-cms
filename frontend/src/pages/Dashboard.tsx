282476d7878d58ec4b842ecd73c20d6bbd39161d
Folareen <sakawahab03@gmail.com>
2024-09-05 12:24:10 +0100

    done for now

---
+++ frontend/src/pages/Dashboard.tsx
@@ -0,0 +1,56 @@
+import { Link } from 'react-router-dom'
+import landingBg from '../assets/landing-bg.png'
+import oauLogo from '../assets/oau-logo.png'
+import { FaRegUser } from 'react-icons/fa6'
+import { LuCalendarClock } from 'react-icons/lu'
+import { FiPhoneCall } from 'react-icons/fi'
+
+const Dashboard = () => {
+    return (
+        <div className='bg-cover min-h-screen w-full font-montserrat' style={{ backgroundImage: `url(${landingBg})` }}>
+            <div className=' min-h-screen w-full bg-[rgba(255,255,255,0.6)] py-12 px-6 '>
+                <Link to='/dashboard' className='flex justify-center items-center space-x-6'>
+                    <img src={oauLogo} className='w-40 h-40 object-contain ' />
+                    <h1 className='text-[rgba(1,0,128,1)] text-[64px] font-montserrat font-semibold text-center leading-[78px]'>
+                        OAU <br /> E-health Centre
+                    </h1>
+                </Link>
+
+                <div className='flex flex-wrap gap-10 justify-center mt-10'>
+                    {
+                        [
+                            {
+                                title: 'Personal Info',
+                                icon: <FaRegUser size={50} />
+                            },
+                            {
+                                title: 'Book an appointment',
+                                icon: <LuCalendarClock size={50} />
+                            },
+                            {
+                                title: 'Contact us',
+                                icon: <LuCalendarClock size={50} />
+                            },
+                            {
+                                title: 'Lab report',
+                                icon: <FiPhoneCall size={50} />
+                            }
+                        ].map(({ title, icon }, index: number) => (
+                            <Link to={`/${title.toLowerCase().split(' ').join('-')}`} className={`${index % 2 == 0 ? 'bg-[#c8d1e2]' : 'bg-[#315d9f]'} flex flex-col p-6 rounded-[8px] w-full md:w-1/2 lg:w-1/4 justify-center items-center gap-2 `}>
+                                {icon}
+                                <p className='font-montserrat text-3xl font-bold'>
+                                    {title}
+                                </p>
+                            </Link>
+                        ))
+                    }
+
+                </div>
+
+
+            </div>
+        </div>
+    )
+}
+
+export default Dashboard
---
+++ frontend/src/pages/Reception.tsx
@@ -0,0 +1,74 @@
+import { FormEvent, useState } from 'react'
+import { toast } from 'react-toastify'
+import landingBg from '../assets/landing-bg.png'
+import oauLogo from '../assets/oau-logo.png'
+import useAuthStore from '../stores/useAuthStore'
+import { Link } from 'react-router-dom'
+import { IoMdNotificationsOutline } from 'react-icons/io'
+
+const Reception = () => {
+    const [name, setName] = useState('')
+    const [address, setAddress] = useState('')
+    const [problem, setProblem] = useState('')
+
+    const [submitting, setSubmitting] = useState(false)
+
+    const { authenticate } = useAuthStore()
+
+    const handleSubmit = async (e: FormEvent) => {
+        try {
+            e.preventDefault()
+            setSubmitting(true)
+        } catch (error: any) {
+            toast.error(error?.message || error)
+        } finally {
+            setSubmitting(false)
+        }
+    }
+
+
+    return (
+        <div className='bg-cover min-h-screen w-full font-montserrat' style={{ backgroundImage: `url(${landingBg})` }}>
+            <div className=' min-h-screen w-full bg-[rgba(255,255,255,0.6)] py-12 px-6 '>
+                <Link to='/dashboard' className='flex justify-center items-center space-x-6'>
+                    <img src={oauLogo} className='w-40 h-40 object-contain ' />
+                    <h1 className='text-[rgba(1,0,128,1)] text-[48px] font-montserrat font-semibold text-center leading-[78px]'>
+                        OAU <br /> E-health Centre
+                    </h1>
+                </Link>
+
+                <div className='w-[70%] mx-auto ' onSubmit={handleSubmit}>
+
+                    <h2 className='text-[40px] text-white bg-[rgba(1,0,128,1)] p-3 text-center font-montserrat font-bold my-5 w-1/2 mx-auto'>
+                        Reception
+                    </h2>
+
+                    <div className='flex flex-wrap justify-between gap-5 mt-20'>
+                        {
+                            [
+                                'Medicine',
+                                'Appointment',
+                                'Patient Documentation',
+                                'Emergency'
+                            ].map((tab) => (
+                                <div className='w-[45%] mt-5'>
+                                    <p className=' font-montserrat font-bold text-4xl mb-1'>
+                                        {tab}
+                                    </p>
+                                    <div className={` w-full rounded-[25px] p-6 text-white flex justify-end `} style={{ backgroundColor: `${tab == 'Medicine' ? 'rgb(1,0,128)' : tab == 'Appointment' ? 'rgba(254, 204, 59, 1)' : tab == 'Patient Documentation' ? '#fecc3b' : 'rgba(255,0,0,1)'}` }}>
+                                        <IoMdNotificationsOutline size={30} />
+                                    </div>
+                                </div>
+                            ))
+                        }
+                    </div>
+
+                </div>
+
+
+            </div>
+        </div >
+    )
+}
+
+export default Reception
